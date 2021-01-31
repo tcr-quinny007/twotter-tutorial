b1f5e59a871aaec06d88e2478c41f6a00a6b8b32
Quincy van Riet <quincyvr@live.nl>
2021-02-01 00:25:33 +0100

    project added

---
+++ twotter-tutorial/src/assets/users.js
@@ -0,0 +1,20 @@
+export const users = [
+    {
+      "id": 1,
+      "username": "realDonaldTrump",
+      "firstName": "Donald",
+      "lastName": "Trump",
+      "email": "donaldjtrump@45office.com",
+      "isAdmin": true,
+      "twoots": []
+    },
+    {
+      "id": 2,
+      "username": "quincysd1",
+      "firstName": "Quincy",
+      "lastName": "van Riet",
+      "email": null,
+      "isAdmin": false,
+      "twoots": []
+    }
+]
